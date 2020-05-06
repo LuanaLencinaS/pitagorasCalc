4128605d01f672809ad3bdf1730ad1ab29d69cdc
Luana Lencina <analulencina@hotmail.com>
2020-05-06 00:22:11 -0300

    Publicando

--- assets/js/particles.js
+++ assets/js/particles.js
@@ -1,7 +1,7 @@
-particlesJS.load('particles-js', './assets/js/particles.json', 
-    function() {
-    console.log('particles.json config loaded...');
-});
+// particlesJS.load('particles-js', './assets/js/particles.json', 
+//     function() {
+//     console.log('particles.json config loaded...');
+// });
 
 // .........................................................
 function pegaCatetoOposto(){
